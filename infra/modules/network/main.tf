d0a5ec887200f1314c767c322b1512dedfbb3164
Arno Schutijzer <arno.schutijzer@gmail.com>
2020-03-01 23:49:08 +0100

    feat(network): add subnets & route tables

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "exposed" {
   }
 }
 
-resource "aws_network_acl" "main" {
+resource "aws_network_acl" "exposed" {
   vpc_id = aws_vpc.exposed.id
 
   egress {
@@ -32,6 +32,51 @@ resource "aws_network_acl" "main" {
   }
 }
 
-output "network_acl_id" {
-  value = aws_network_acl.main
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id     = aws_vpc.exposed.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public subnet A"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id     = aws_vpc.exposed.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public subnet B"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.exposed.id
+
+  tags = {
+    Name = "exposed internet gateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.exposed.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "exposed public routetable"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_a_route_table" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_b_route_table" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_route_table.id
 }
