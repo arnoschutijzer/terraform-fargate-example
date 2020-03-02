e885cd3e67f29ef2a186339db12ba3d014c8f696
Arno Schutijzer <arno.schutijzer@gmail.com>
2020-03-02 17:24:46 +0100

    feat: add /id endpoint

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,16 @@
 import express = require('express')
+import uuid = require('uuid');
 
 const app: express.Application = express()
 const PORT: number = 80
+const id = uuid.v4()
 
 app.get('/', (req, res) => {
   res.send(process.env)
+})
+
+app.get('/id', (req, res) => {
+  res.send({ id })
 })
 
 app.listen(PORT, () => {
