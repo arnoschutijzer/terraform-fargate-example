e2eb35fd33540072f74155edde7f5de5ea169aa8
Arno Schutijzer <arno.schutijzer@gmail.com>
2020-03-02 17:09:33 +0100

    feat: add private subnets and NAT gateway

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -86,3 +86,59 @@ resource "aws_route_table_association" "public_subnet_b_route_table" {
   subnet_id      = aws_subnet.public_subnet_b.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_eip" "nat_gateway_ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_gateway_ip.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+
+  tags = {
+    Name = "exposed NAT"
+  }
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id     = aws_vpc.exposed.id
+  cidr_block = "10.0.10.0/24"
+
+  tags = {
+    Name = "private subnet A"
+  }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+  vpc_id     = aws_vpc.exposed.id
+  cidr_block = "10.0.11.0/24"
+
+  tags = {
+    Name = "private subnet B"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.exposed.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "exposed private routetable"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_a_route_table" {
+  subnet_id      = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_b_route_table" {
+  subnet_id      = aws_subnet.private_subnet_b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
