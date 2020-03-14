8164d121d6dc5d3a1a041cf41887b036338c5b09
Arno Schutijzer <arno.schutijzer@gmail.com>
2020-03-14 23:22:30 +0100

    feat(lb): add default http listener

--- infra/modules/loadbalancer/main.tf
+++ infra/modules/loadbalancer/main.tf
@@ -33,3 +33,18 @@ resource "aws_security_group" "lb_sg" {
     Name = "${var.identifier}-public-lb"
   }
 }
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "400"
+    }
+  }
+}
--- infra/modules/loadbalancer/outputs.tf
+++ infra/modules/loadbalancer/outputs.tf
@@ -5,3 +5,7 @@ output "lb_id" {
 output "lb_security_group" {
   value = aws_security_group.lb_sg.id
 }
+
+output "lb_http_listener_arn" {
+  value = aws_lb_listener.http_listener.arn
+}
