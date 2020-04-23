85c1847e2aa83af0bab8ecad74fa98002670eacc
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-04-23 18:27:13 +0200

    chore: remove terraform example

--- infra/test/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "exposed.tf.state-bucket"
-    key    = "test"
-    region = "eu-west-1"
-  }
-}
--- infra/test/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "identifier" {
-  type = string
-  default = "exposed-test"
-}
-
-variable "region" {
-  type = string
-  default = "eu-west-1"
-}
