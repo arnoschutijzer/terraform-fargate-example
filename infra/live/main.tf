e72ff91975c731d459c9078345631a8938aa0404
Arno Schutijzer <arno.schutijzer@gmail.com>
2020-06-10 01:24:39 +0200

    feat: add live infrastructure

---
+++ infra/live/main.tf
@@ -0,0 +1,16 @@
+module "network" {
+  source = "../modules/network"
+}
+
+module "load_balancer" {
+  source     = "../modules/loadbalancer"
+  lb_subnets = module.network.public_subnet_ids
+  vpc_id     = module.network.vpc_id
+}
+
+module "fargate" {
+  source               = "../modules/fargate"
+  subnets              = module.network.private_subnet_ids
+  vpc_id               = module.network.vpc_id
+  target_group_arn     = module.load_balancer.target_group_arn
+}
